99051bee5841145e44698170fe7ca83c3d331d48
Kay B <kaylouisebennett@gmail.com>
2018-09-27 21:29:05 +0000

    Done.

--- db/migrate/20140922151507_create_ponds.rb
+++ db/migrate/20140922151507_create_ponds.rb
@@ -1,3 +1,12 @@
 class CreatePonds < ActiveRecord::Migration
   # code goes here
+  def change
+    create_table :ponds do |t|
+      t.string :name
+      t.string :water_type
+      t.references :frog
+      t.references :tadpole
+      t.timestamps
+    end
+  end
 end
